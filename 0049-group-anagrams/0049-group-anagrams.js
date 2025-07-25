fd73190c9ca98a3ac7c360b7fed2e9db4e818feb
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-07-25 19:12:06 +0530

    Time: 130 ms (5.05%), Space: 73.5 MB (5.44%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    res = {}
+    for (const s of strs) {
+        const count = new Array(26).fill(0);
+        for (const e of s) {
+            count[e.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        }       
+        if(!res[count]){
+            res[count]=[]
+        }
+        res[count].push(s)
+    }
+    console.log(res)
+    return Object.values(res)
+};
