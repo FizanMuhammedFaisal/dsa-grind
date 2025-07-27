457627310d006d5d22c6e125f7bffee2eecb7141
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-07-27 18:52:45 +0530

    Time: 13 ms (53.39%), Space: 61.4 MB (35.48%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -9,18 +9,17 @@ var topKFrequent = function (nums, k) {
         hash[n] = (hash[n] || 0) + 1
     }
     const arr = Array.from({ length: nums.length + 1 }, () => [])
-
     for (const n of Object.keys(hash)) {
-        arr[hash[n]].push(n)
+        arr[hash[n]].push(parseInt(n))
     }
     const result = []
-
     for (let i=arr.length-1;i>0;i--) {
       const e=arr[i]
         if (arr[i].length && result.length < k) {
-            for(let j=0;j<arr[i].length;j++){
-                if(result.length<k){
-                    result.push(Number(arr[i][j]))
+            for(const n of arr[i]){
+                 result.push(n)
+                if(result.length===k){
+                   return result
                 }
             }
         }
