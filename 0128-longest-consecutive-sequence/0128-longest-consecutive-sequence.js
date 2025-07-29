5344c9de86d2909fcc59bc00a871a4a85512ad3b
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-07-29 19:00:14 +0530

    Time: 41 ms (49.86%), Space: 80.9 MB (50.63%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,18 +3,22 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
- const numSet = new Set(nums);
-        let longest = 0;
+    const set = new Set(nums)
 
-        for (let num of numSet) {
-            if (!numSet.has(num - 1)) {
-                let length = 1;
-                while (numSet.has(num + length)) {
-                    length++;
-                }
-                longest = Math.max(longest, length);
+    let longest = 0
+
+    for (const num of set) {
+        if (!set.has(num - 1)) {
+            let length = 1;
+            while (set.has(num + length)) {
+                length++;
             }
+            longest = Math.max(longest, length);
         }
-        return longest;
+    }
+
+    return longest
+
+
 
 };
