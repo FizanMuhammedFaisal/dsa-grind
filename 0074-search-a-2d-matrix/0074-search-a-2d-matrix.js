cd133a474f12c5daf95aec2bccab69d907f7ed5c
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-08-18 19:54:07 +0530

    Time: 4 ms (6.25%), Space: 55.5 MB (5.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -7,7 +7,6 @@ var searchMatrix = function (matrix, target) {
     const row = matrix.length
     const col = matrix[0].length
 
-    //find which row
     let top = 0
     let bot = row - 1
     while (top <= bot) {
@@ -27,19 +26,13 @@ var searchMatrix = function (matrix, target) {
             return false;
         }
         const ind = Math.floor((top + bot) / 2);
-
         const tar = matrix[ind]
-
-        console.log(tar)
         let l = 0
         let r = tar.length - 1
-console.log(l+'l',r+'r')
-console.log(target)
         while (l <= r) {
             const mid = Math.floor((l + r) / 2)
             console.log(mid)
             if (tar[mid] > target) {
-                console.log('enterd')
                 r = mid - 1
             } else if (tar[mid] < target) {
                 l = mid + 1
