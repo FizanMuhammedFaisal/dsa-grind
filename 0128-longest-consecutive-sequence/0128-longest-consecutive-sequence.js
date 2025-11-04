9580ee22bdeb5f1d7c0b4141371cad28d3f95a9e
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-11-04 10:32:30 +0530

    Time: 40 ms (47.96%), Space: 80.2 MB (50.82%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -2,23 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function (nums) {
-    const set = new Set(nums)
+var longestConsecutive = function(nums) {
+    // make a hashtable 
+    // see if a elemnt have elment-1  in arr  
+    // then if ther is then it not inital element of a consicutive list if ther is then 
+    // we can look if upcoming next numbers are there in arr, take the longest such arr length and reuturn
 
-    let longest = 0
 
-    for (const num of set) {
-        if (!set.has(num - 1)) {
-            let length = 1;
-            while (set.has(num + length)) {
-                length++;
+    const hash=new Set(nums)
+    let longest=0
+    for(const num of hash){
+        if(!hash.has(num-1)){
+            let curr=0
+            while(hash.has(num+curr)){
+                curr++
             }
-            longest = Math.max(longest, length);
+           longest = Math.max(longest, curr);
         }
     }
-
     return longest
-
-
-
 };
