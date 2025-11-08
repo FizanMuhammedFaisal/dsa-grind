afd2bc9e12a78b00f4eff835fb05a7de76068c18
Fizan Muhammed Faisal <145630603+FizanMuhammedFaisal@users.noreply.github.com>
2025-11-08 20:02:33 +0530

    Time: 189 ms (7.57%), Space: 65.7 MB (5.01%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -3,31 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-    
+var twoSum = function (numbers, target) {
 
-    let curr=0
-    let l=0
-    let r=numbers.length-1
+    let r = numbers.length - 1
+    let l = 0
 
-    // for(let i=0;i<numbers.length;i++){
-    //         curr=numbers[i]
-    //         l=i
-    //         r=i+1
-    //     while( l < r && r<=numbers.length){
-    //         if(curr+numbers[r]===target){
-    //             return [i+1,r+1]
-    //         }
-    //         r++
-    //     }
-    // }
-    while(l<r){
-        if(numbers[l]+numbers[r]===target){
-            return [l+1,r+1]
+    while (l < r) {
+
+        const n = numbers[r] + numbers[l]
+        console.log(n)
+        if (n === target) {
+            return [l+1 , r+1 ]
         }
-        if(numbers[l]+numbers[r]>target){
+        if (n > target) {
             r--
-        }else{
+        } else {
             l++
         }
     }
